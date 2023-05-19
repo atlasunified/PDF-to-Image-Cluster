8e5f19eb6160535cb465031574bc0b666a4b7c56
Atlas Unified <113645143+atlasunified@users.noreply.github.com>
2023-05-18 21:04:19 -0400

    Update 000-downloader.py

--- 000-downloader.py
+++ 000-downloader.py
@@ -1,29 +1,36 @@
 import os
-import pandas as pd
-import pyarrow.parquet as pq
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-def parquet_to_csv(parquet_filepath):
-    try:
-        # Generate the csv filepath
-        csv_filepath = parquet_filepath.replace('.parquet', '.csv')
-        
-        # Read the parquet file
-        dataframe = pd.read_parquet(parquet_filepath)
-        
-        # Write to a csv file
-        dataframe.to_csv(csv_filepath, index=False)
-        print(f'Successfully converted {parquet_filepath} to {csv_filepath}')
-        
-        # Remove the parquet file
-        os.remove(parquet_filepath)
-        print(f'Successfully deleted {parquet_filepath}')
-    except Exception as e:
-        print(f'An error occurred: {e}')
+def download_file(url, download_path):
+    local_filename = url.split('/')[-1]
+    local_filename = os.path.join(download_path, local_filename)
+    
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+    return local_filename
 
-# Directory containing the parquet files
-directory = 'cc-data'
+def scrape_and_download(base_url, page_url, download_path):
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        if url and '.snappy.parquet' in url:
+            full_url = urljoin(base_url, url)
+            print(f'Downloading {full_url}')
+            download_file(full_url, download_path)
 
-# Convert all parquet files in the directory to csv and delete them
-for filename in os.listdir(directory):
-    if filename.endswith('.parquet'):
-        parquet_to_csv(os.path.join(directory, filename))
+base_url = 'http://3080.rom1504.fr/n/text/text38M/'
+page_url = 'http://3080.rom1504.fr/n/text/text38M/'
+download_path = 'cc-data'  # modify this as per your requirements
+
+# Create target Directory if it doesn't exist
+if not os.path.exists(download_path):
+    os.mkdir(download_path)
+    print("Directory " , download_path ,  " created ")
+
+scrape_and_download(base_url, page_url, download_path)
